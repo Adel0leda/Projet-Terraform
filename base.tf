4de201ad4c162f45ef24504bcc4f25e043ebb781
Adel0leda <65973943+Adel0leda@users.noreply.github.com>
2021-07-07 17:23:37 +0200

    Update base.tf

--- base.tf
+++ base.tf
@@ -10,3 +10,11 @@ provider "azurerm" {
   features{}
 }
 
+# Create a resource group
+resource "azurerm_resource_group" "RG1" {
+  name     = var.rgName
+  location = var.region
+  tags = {
+        environment =  var.tag
+    }
+}
